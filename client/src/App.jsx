13575663d06fd01a83171c5fbfe3843b9dee66a7
Hikari Sakamoto <hikari@townsq.com.br>
2021-05-24 13:25:06 -0300

    Finishing up general chat implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "shards-ui/dist/css/shards.min.css";
 
-import "./index.css";
+import './index.css';
 import Chat from './Chat';
  
 const App = () => <Chat/>;
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache, useQuery, gql } from '@apollo/client';
+import {ApolloClient, ApolloProvider, gql, InMemoryCache, useMutation, useQuery} from '@apollo/client';
+import {Button, Col, Container, FormInput, Row} from "shards-react";
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/',
@@ -7,32 +8,137 @@ const client = new ApolloClient({
 });
 
 const GET_MESSAGES = gql`
-query {
-    message {
-        id
-        content
-        user
+    query {
+        message {
+            id
+            content
+            user
+        }
     }
-}
 `;
 
-const Messages = ({ user }) => {
-    const { data } = useQuery(GET_MESSAGES);
+const POST_MESSAGE = gql`
+    mutation ($user:String!, $content:String!){
+        postMessage(user:$user, content: $content)
+    }
+`;
+
+const Messages = ({user}) => {
+    const {data} = useQuery(GET_MESSAGES);
     if (!data) {
         null;
     }
 
-    return JSON.stringify(data);
-}
+    return (
+        <>
+            {data.messages.map(({id, user: messageUser, content}) => (
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+                            paddingBottom: '1em',
+                        }}>
+                        {user !== messageUser && (
+                            <div
+                                style={{
+                                    height: 50,
+                                    width: 50,
+                                    marginRight: '0.5em',
+                                    border: '2px solid #e5e6ae',
+                                    borderRadius: 25,
+                                    textAlign: 'center',
+                                    fontSize: '18pt',
+                                    paddingTop: 5,
+                                }}
+                            >
+                                {messageUser.slice(0, 2).toUpperCase()}
+                            </div>
+                        )}
+                        <div
+                            style={{
+                                background: user === messageUser ? '#58bf56' : '#e5e6a',
+                                color: user === messageUser ? 'white' : 'black',
+                                padding: '1em',
+                                borderRadius: '1em',
+                                maxWidth: '60%',
+                            }}
+                        >
+                            {content}
+                        </div>
+                    </div>
+                )
+            )}
+        </>
+    )
+};
 
 const Chat = () => {
+    const [state, stateSet] = React.useState({
+        user: 'Hikari',
+        content: '',
+    });
+
+    const [postMessage] = useMutation(POST_MESSAGE);
+
+    const onSend = () => {
+        if (state.content.length > 0) {
+            postMessage({
+                variables: state,
+            });
+
+            stateSet({
+                ...state,
+                content: '',
+
+            });
+        }
+    };
+
+
     return (
-        <div> <Messages user="Hikari" /></div>
+        <Container>
+            <Messages user={state.user}/>
+            <Row>
+                <Col xs={2} style={{padding: 0}}>
+                    <FormInput
+                        label="User"
+                        value={state.user}
+                        onChange={(evt) => stateSet({
+                            ...state,
+                            user: evt.target.value,
+                        })}
+                    />
+                </Col>
+                <Col xs={8}>
+                    <FormInput
+                        label="Content"
+                        value={state.content}
+                        onChange={(evt) => stateSet({
+                            ...state,
+                            user: evt.target.value,
+                        })
+                        }
+                        onKeyUp={(evt) => {
+                            if (evt.keyCode === 13) {
+                                onSend();
+                            }
+                        }}
+                    />
+                </Col>
+                <Col xs={2} style={{padding: 0}}>
+                    <Button onClick={() => onSend()}>
+                        Send
+                    </Button>
+
+                </Col>
+            </Row>
+        </Container>
     )
-}
+
+};
 
 export default () => (
     <ApolloProvider client={client}>
-        <Chat />
+        <Chat/>
     </ApolloProvider>
-)
+);
